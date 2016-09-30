0605bc97ebc7204fa34c4346e19a1f6dd0bbde68
Christian Rotzoll <ch.rotzoll@gmail.com>
2016-09-30 22:37:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tornado-Todolist
+
+Currently: WIP
+
+I've also build a quite similar app using:
+* [flask](https://github.com/polyfunc/flask-todolist)
+* [django](https://github.com/polyfunc/django-todolist)
---
+++ todolist.py
@@ -0,0 +1,18 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('ready for some todolists')
+
+
+def make_app():
+    return tornado.web.Application([
+        (r'/', MainHandler),
+    ])
+
+if __name__ == '__main__':
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
