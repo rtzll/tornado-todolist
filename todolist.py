0b6b888aaed694856fd236524da4e2e1e303526b
Christian Rotzoll <ch.rotzoll@gmail.com>
2016-10-03 19:33:23 +0200

    Add login/logout handling
    Currently only based on hardcoded credentials, until DB/ a user model is
    in place.

--- todolist.py
+++ todolist.py
@@ -14,14 +14,17 @@ BASEDIR = os.path.dirname(__file__)
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r'/', MainHandler),
-            (r'/login', LoginHandler),
+            tornado.web.url(r'/', MainHandler, name='main'),
+            tornado.web.url(r'/register', RegisterHandler, name='register'),
+            tornado.web.url(r'/login', LoginHandler, name='login'),
+            tornado.web.url(r'/logout', LogoutHandler, name='logout'),
         ]
         settings = {
             'static_path':  os.path.join(BASEDIR, 'static'),
             'template_path': os.path.join(BASEDIR, 'templates'),
             'debug': True,
             'cookie_secret': os.environ.get('SECRET_KEY') or 'testing key',
+            'xsrf_cookies': True,
         }
         super().__init__(handlers=handlers, **settings)
 
@@ -30,9 +33,29 @@ class BaseHandler(tornado.web.RequestHandler):
     pass
 
 
+class RegisterHandler(BaseHandler):
+    # TODO
+    pass
+
+
 class LoginHandler(BaseHandler):
     def get(self):
         self.render('login.html')
+
+    def post(self):
+        username = tornado.escape.xhtml_escape(self.get_argument("username"))
+        password = tornado.escape.xhtml_escape(self.get_argument("password"))
+        # TODO replace when DB/User model is done
+        if "demo" == username and "demo" == password:
+            self.set_secure_cookie("user", self.get_argument("username"))
+            self.redirect(self.get_argument('next', self.reverse_url('main')))
+        self.render('login.html')
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie('user')
+        self.redirect(self.get_argument('next', self.reverse_url('main')))
 
 
 class MainHandler(BaseHandler):
