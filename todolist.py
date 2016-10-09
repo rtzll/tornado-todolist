9bb53b03e2c7e81b7ee61d995208d3555075fde6
Christian Rotzoll <ch.rotzoll@gmail.com>
2016-10-09 10:10:05 +0200

    Add get_current_user to base handler

--- todolist.py
+++ todolist.py
@@ -54,6 +54,9 @@ class BaseHandler(tornado.web.RequestHandler):
     def db(self):
         return self.application.db
 
+    def get_current_user(self):
+        return self.get_secure_cookie('user')
+
 
 class RegisterHandler(BaseHandler):
     def get(self):
